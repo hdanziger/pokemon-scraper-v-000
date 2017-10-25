bc71f1611b6632c3fa0751dc24cae0a40edbefba
Hannah Danziger <hdanziger8@gmail.com>
2017-10-25 15:09:11 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -16,7 +16,7 @@ class Pokemon
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
   end
 
-  def self.find
+  def self.find(name, type)
     db.execute("SELECT")
   end
 end
