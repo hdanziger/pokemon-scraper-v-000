d217d421f80f15353a445c29c19ca0c4b67d806e
Hannah Danziger <hdanziger8@gmail.com>
2017-10-25 16:06:03 +0000

    automatically backed up by learn

--- db/alter_table_migration.sql
+++ db/alter_table_migration.sql
@@ -2,4 +2,4 @@ ALTER TABLE pokemon ADD COLUMN hp INTEGER;
 
 INSERT INTO pokemon (hp) VALUES (60);
 
-UPDATE pokemon SET hp = 59 WHERE name = "pikachu";
+UPDATE pokemon SET hp = 59 WHERE name = "Pikachu";
