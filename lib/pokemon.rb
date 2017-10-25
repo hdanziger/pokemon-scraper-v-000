f1c5ba1f5ea4d6b2a83327636e83dc78318fc09c
Hannah Danziger <hdanziger8@gmail.com>
2017-10-25 14:50:57 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,9 +1,17 @@
 class Pokemon
   #build methods
+  attr_accessor :id, :name, :type, :db
 
   def initialize
+    @id = id 
+    @name = name 
+    @type = type 
+    @db = db 
   end
 
   def save
+  end
+
+  def find
   end
 end
